648259b8d9ba7e46acc19820e810f15f1bdb1daa
cdclaxton <c.d.claxton@gmail.com>
2022-08-02 08:28:57 +0100

    Corrected incomplete test.

--- set/set_test.go
+++ set/set_test.go
@@ -1,7 +1,6 @@
 package set
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -55,8 +54,24 @@ func TestIntersection(t *testing.T) {
 }
 
 func TestString(t *testing.T) {
-	s1 := NewSet[int]()
-	s1.AddAll([]int{1, 2, 3})
-	fmt.Println(s1)
-	assert.True(t, false)
+	s1 := NewSet[string]()
+	s1.AddAll([]string{"A", "B", "C"})
+
+	assert.True(t, s1.Has("A"))
+	assert.True(t, s1.Has("B"))
+	assert.True(t, s1.Has("C"))
+	assert.False(t, s1.Has("D"))
+
+	s2 := NewSet[string]()
+	s2.AddAll([]string{"A", "B", "C"})
+	assert.True(t, s1.Equal(&s2))
+
+	s3 := NewSet[string]()
+	s3.AddAll([]string{"A", "B", "D", "E"})
+	assert.False(t, s1.Equal(&s3))
+
+	s1_3 := s1.Intersection(&s3)
+	expected := NewSet[string]()
+	expected.AddAll([]string{"A", "B"})
+	assert.True(t, s1_3.Equal(&expected))
 }
