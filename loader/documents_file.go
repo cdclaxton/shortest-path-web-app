a0b5ce7b7c035e5de88cc7d894824378cd7abb16
cdclaxton <c.d.claxton@gmail.com>
2022-08-06 08:42:05 +0100

    Added the Documents file reader.

--- loader/documents_file.go
+++ loader/documents_file.go
@@ -1,11 +1,179 @@
 package loader
 
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cdclaxton/shortest-path-web-app/graphstore"
+)
+
 type DocumentsCsvFile struct {
 	Path             string            // Location of the file
-	EntityType       string            // Type of documents in the file
+	DocumentType     string            // Type of documents in the file
 	Delimiter        string            // Delimiter
-	IsEncapsulated   bool              // Is an encapsulator used?
-	Encapsulator     string            // Encapsulator (if used)
 	DocumentIdField  string            // Name of the field with the document ID
 	FieldToAttribute map[string]string // Mapping of field name to attribute
+}
+
+func NewDocumentsCsvFile(path string, documentType string, delimiter string,
+	documentIdField string, fieldToAttribute map[string]string) DocumentsCsvFile {
+
+	return DocumentsCsvFile{
+		Path:             path,
+		DocumentType:     documentType,
+		Delimiter:        delimiter,
+		DocumentIdField:  documentIdField,
+		FieldToAttribute: fieldToAttribute,
+	}
+}
+
+type DocumentsCsvFileReader struct {
+	documentsCsvFile     DocumentsCsvFile
+	csvReader            *csv.Reader
+	file                 *os.File
+	documentIdFieldIndex int
+	attributeFieldIndex  map[string]int
+
+	nextDocument graphstore.Document
+	hasNext      bool
+}
+
+func NewDocumentsCsvFileReader(csv DocumentsCsvFile) *DocumentsCsvFileReader {
+	return &DocumentsCsvFileReader{
+		documentsCsvFile: csv,
+	}
+}
+
+func (reader *DocumentsCsvFileReader) Initialise() error {
+
+	// Open the file
+	var err error
+	reader.file, err = os.Open(reader.documentsCsvFile.Path)
+	if err != nil {
+		return err
+	}
+
+	// Create the CSV reader
+	reader.csvReader = csv.NewReader(reader.file)
+
+	// Read the header from the file
+	header, err := reader.csvReader.Read()
+
+	// Find the document ID field index
+	fieldToIndex, err := findIndicesOfFields(header, []string{reader.documentsCsvFile.DocumentIdField})
+
+	if err != nil {
+		return err
+	}
+
+	reader.documentIdFieldIndex = fieldToIndex[reader.documentsCsvFile.DocumentIdField]
+
+	// Create a mapping from the attribute to the field index in the CSV file
+	reader.attributeFieldIndex, err = attributeToFieldIndex(header, reader.documentsCsvFile.FieldToAttribute)
+
+	if err != nil {
+		return err
+	}
+
+	// Read the first record
+	reader.nextDocument, reader.hasNext = reader.readRecord()
+
+	return nil
+}
+
+func (reader *DocumentsCsvFileReader) readRecord() (graphstore.Document, bool) {
+
+	recordFound := false
+	var record []string
+	var document graphstore.Document
+
+	for !recordFound {
+		var err error
+		record, err = reader.csvReader.Read()
+
+		if err == io.EOF {
+			// End of file
+			return graphstore.Document{}, false
+		}
+
+		if err != nil {
+			// TODO log message
+			continue
+		}
+
+		// Extract the document ID
+		documentId := record[reader.documentIdFieldIndex]
+
+		// Extract the document attributes
+		attributes, err := extractAttributes(record, reader.attributeFieldIndex)
+
+		if err != nil {
+			// TODO log message
+			continue
+		}
+
+		// Build the document
+		document, err = graphstore.NewDocument(documentId, reader.documentsCsvFile.DocumentType,
+			attributes)
+
+		if err != nil {
+			// TODO log message
+			continue
+		}
+
+		recordFound = true
+	}
+
+	return document, true
+}
+
+// Next Document from the file.
+func (reader *DocumentsCsvFileReader) Next() (graphstore.Document, error) {
+
+	if !reader.hasNext {
+		return graphstore.Document{}, fmt.Errorf("Next() called when no next item exists")
+	}
+
+	// Get the current Document
+	current := reader.nextDocument
+
+	// Try to read the next record
+	reader.nextDocument, reader.hasNext = reader.readRecord()
+
+	return current, nil
+}
+
+// Close the documents CSV file
+func (reader *DocumentsCsvFileReader) Close() error {
+	return reader.file.Close()
+}
+
+// ReadAll the documents from the CSV file.
+func (reader *DocumentsCsvFileReader) ReadAll() ([]graphstore.Document, error) {
+
+	// Initialise the CSV readers
+	err := reader.Initialise()
+	if err != nil {
+		return nil, err
+	}
+
+	// Read all the documents from the file
+	documents := []graphstore.Document{}
+
+	for reader.hasNext {
+		document, err := reader.Next()
+
+		if err != nil {
+			return documents, err
+		}
+
+		documents = append(documents, document)
+	}
+
+	// Close the reader
+	err = reader.Close()
+
+	return documents, err
 }
---
+++ loader/documents_file_test.go
@@ -0,0 +1,127 @@
+package loader
+
+import (
+	"testing"
+
+	"github.com/cdclaxton/shortest-path-web-app/graphstore"
+	"github.com/cdclaxton/shortest-path-web-app/set"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDocumentsHeaderOnly(t *testing.T) {
+
+	csv := NewDocumentsCsvFile("./test-data/documents_0.csv", "Source-1",
+		",", "document_id", map[string]string{
+			"title": "Title",
+			"date":  "Date",
+		})
+
+	reader := NewDocumentsCsvFileReader(csv)
+
+	err := reader.Initialise()
+	assert.NoError(t, err)
+
+	assert.Equal(t, 0, reader.documentIdFieldIndex)
+	assert.Equal(t, map[string]int{
+		"Title": 1,
+		"Date":  2,
+	}, reader.attributeFieldIndex)
+
+	assert.False(t, reader.hasNext)
+
+	assert.NoError(t, reader.Close())
+}
+
+func TestReadDocumentsFile(t *testing.T) {
+	testCases := []struct {
+		csv               DocumentsCsvFile
+		expectedDocuments []graphstore.Document
+		expectedError     bool
+	}{
+		{
+			csv: DocumentsCsvFile{
+				Path:            "./test-data/documents_0.csv",
+				DocumentType:    "Source-1",
+				Delimiter:       ",",
+				DocumentIdField: "document_id",
+				FieldToAttribute: map[string]string{
+					"title": "Title",
+					"date":  "Date",
+				},
+			},
+			expectedDocuments: []graphstore.Document{},
+			expectedError:     false,
+		},
+		{
+			csv: DocumentsCsvFile{
+				Path:            "./test-data/documents_1.csv",
+				DocumentType:    "Source-1",
+				Delimiter:       ",",
+				DocumentIdField: "document_id",
+				FieldToAttribute: map[string]string{
+					"title": "Title",
+					"date":  "Date",
+				},
+			},
+			expectedDocuments: []graphstore.Document{
+				{
+					Id:           "d-1",
+					DocumentType: "Source-1",
+					Attributes: map[string]string{
+						"Title": "A summary of activity",
+						"Date":  "06/08/2022",
+					},
+					LinkedEntityIds: set.NewSet[string](),
+				},
+			},
+			expectedError: false,
+		},
+		{
+			csv: DocumentsCsvFile{
+				Path:            "./test-data/documents_2.csv",
+				DocumentType:    "Source-1",
+				Delimiter:       ",",
+				DocumentIdField: "document_id",
+				FieldToAttribute: map[string]string{
+					"title": "Title",
+					"date":  "Date",
+				},
+			},
+			expectedDocuments: []graphstore.Document{
+				{
+					Id:           "d-1",
+					DocumentType: "Source-1",
+					Attributes: map[string]string{
+						"Title": "A summary of activity",
+						"Date":  "06/08/2022",
+					},
+					LinkedEntityIds: set.NewSet[string](),
+				},
+				{
+					Id:           "d-2",
+					DocumentType: "Source-1",
+					Attributes: map[string]string{
+						"Title": "New information",
+						"Date":  "07/08/2022",
+					},
+					LinkedEntityIds: set.NewSet[string](),
+				},
+			},
+			expectedError: false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		reader := NewDocumentsCsvFileReader(testCase.csv)
+
+		documents, err := reader.ReadAll()
+
+		if testCase.expectedError {
+			assert.Error(t, err)
+		} else {
+			assert.NoError(t, err)
+		}
+
+		assert.Equal(t, testCase.expectedDocuments, documents)
+	}
+}
