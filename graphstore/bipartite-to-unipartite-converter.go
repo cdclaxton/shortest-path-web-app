3557ca413c5656618efb6974df5c869a5caa0837
cdclaxton <c.d.claxton@gmail.com>
2022-08-14 09:27:11 +0100

    Added functionality to skip entities.

--- graphstore/bipartite-to-unipartite-converter.go
+++ graphstore/bipartite-to-unipartite-converter.go
@@ -1,9 +1,17 @@
 package graphstore
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/cdclaxton/shortest-path-web-app/set"
+)
 
 // BipartiteToUnipartite converter to load a unipartite graph from a bipartite graph.
-func BipartiteToUnipartite(bi BipartiteGraphStore, uni UnipartiteGraphStore) error {
+//
+// The set of skipEntities are those entities that won't be transferred to the
+// unipartite graph.
+func BipartiteToUnipartite(bi BipartiteGraphStore, uni UnipartiteGraphStore,
+	skipEntities *set.Set[string]) error {
 
 	// Iterator to retrieve documents from the bipartite graph store
 	it := bi.NewDocumentIdIterator()
@@ -22,23 +30,33 @@ func BipartiteToUnipartite(bi BipartiteGraphStore, uni UnipartiteGraphStore) err
 		// Entity IDs related to the document
 		entityIds := doc.LinkedEntityIds.ToSlice()
 
-		// Add the entity/entities to the graph
+		// If there is just a single entity, add it to the graph
 		if len(entityIds) == 1 {
 			uni.AddEntity(entityIds[0])
+			continue
+		}
 
-		} else if len(entityIds) > 1 {
-			for _, e1 := range entityIds {
-				for _, e2 := range entityIds {
-					if e1 == e2 {
-						continue
-					}
+		// Add the entities to the graph
+		for _, e1 := range entityIds {
 
+			if skipEntities.Has(e1) {
+				continue
+			}
+
+			for _, e2 := range entityIds {
+
+				if skipEntities.Has(e2) {
+					continue
+				}
+
+				if e1 != e2 {
 					// Add the link
 					err := uni.AddUndirected(e1, e2)
 					if err != nil {
 						return err
 					}
 				}
+
 			}
 		}
 
--- graphstore/bipartite-to-unipartite-converter_test.go
+++ graphstore/bipartite-to-unipartite-converter_test.go
@@ -19,9 +19,13 @@ import (
 //              [e-4]                              |
 //                |                                |
 //             [doc-4] --- [e-5] --- [doc-6] --- [e-6]
+//
+// The test case with entities that are skipped uses the same graph structure,
+// but entity e-2 is ignored.
 func TestBipartiteToUnipartite(t *testing.T) {
 	testCases := []struct {
 		documents           []Document
+		skipEntities        set.Set[string]
 		expectedConnections []connection
 	}{
 		// One document, one entity
@@ -33,6 +37,7 @@ func TestBipartiteToUnipartite(t *testing.T) {
 						"e-1"}),
 				},
 			},
+			skipEntities: set.Set[string]{},
 			expectedConnections: []connection{
 				{
 					source:       "e-1",
@@ -49,6 +54,7 @@ func TestBipartiteToUnipartite(t *testing.T) {
 						"e-1", "e-2"}),
 				},
 			},
+			skipEntities: set.Set[string]{},
 			expectedConnections: []connection{
 				{
 					source:       "e-1",
@@ -94,6 +100,7 @@ func TestBipartiteToUnipartite(t *testing.T) {
 						"e-5", "e-6"}),
 				},
 			},
+			skipEntities: set.Set[string]{},
 			expectedConnections: []connection{
 				{
 					source:       "e-1",
@@ -121,6 +128,64 @@ func TestBipartiteToUnipartite(t *testing.T) {
 				},
 			},
 		},
+		// Big test case with skipped entities
+		{
+			documents: []Document{
+				{
+					Id: "doc-1",
+					LinkedEntityIds: set.NewPopulatedSet([]string{
+						"e-1", "e-2"}),
+				},
+				{
+					Id: "doc-2",
+					LinkedEntityIds: set.NewPopulatedSet([]string{
+						"e-1", "e-2", "e-4"}),
+				},
+				{
+					Id: "doc-3",
+					LinkedEntityIds: set.NewPopulatedSet([]string{
+						"e-2", "e-3"}),
+				},
+				{
+					Id: "doc-4",
+					LinkedEntityIds: set.NewPopulatedSet([]string{
+						"e-4", "e-5"}),
+				},
+				{
+					Id: "doc-5",
+					LinkedEntityIds: set.NewPopulatedSet([]string{
+						"e-3", "e-6"}),
+				},
+				{
+					Id: "doc-6",
+					LinkedEntityIds: set.NewPopulatedSet([]string{
+						"e-5", "e-6"}),
+				},
+			},
+			skipEntities: set.NewPopulatedSet([]string{"e-2"}),
+			expectedConnections: []connection{
+				{
+					source:       "e-1",
+					destinations: []string{"e-4"},
+				},
+				{
+					source:       "e-3",
+					destinations: []string{"e-6"},
+				},
+				{
+					source:       "e-4",
+					destinations: []string{"e-1", "e-5"},
+				},
+				{
+					source:       "e-5",
+					destinations: []string{"e-4", "e-6"},
+				},
+				{
+					source:       "e-6",
+					destinations: []string{"e-3", "e-5"},
+				},
+			},
+		},
 	}
 
 	for _, testCase := range testCases {
@@ -133,7 +198,7 @@ func TestBipartiteToUnipartite(t *testing.T) {
 		}
 
 		// Convert bipartite graph to unipartite graph
-		assert.NoError(t, BipartiteToUnipartite(bi, uni))
+		assert.NoError(t, BipartiteToUnipartite(bi, uni, &testCase.skipEntities))
 
 		// Check the unipartite graph
 		checkConnections(t, uni, testCase.expectedConnections)
