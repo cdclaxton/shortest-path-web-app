485b4868fcf8f040ec927ce137d3103ce59e8195
cdclaxton <c.d.claxton@gmail.com>
2022-10-22 08:02:15 +0100

    Modified script to test and build.

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+# Run the tests
+echo Running tests ...
+go test ./...
+testResult=$?
+
+if [[ $testResult -ne 0 ]]; then
+    echo Tests failed. Stopping build.
+    exit
+fi
+
+# Build the executable
+echo Building executable ...
+go build -v -o ./app ./cmd/app
+buildResult=$?
+
+if [[ $buildResult -ne 0 ]]; then
+    echo Build failed
+else
+    echo Build succeeded
+fi
