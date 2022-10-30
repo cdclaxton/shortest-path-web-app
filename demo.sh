e8c09c2532b6e56e9ef3d401351e32d542cee67f
cdclaxton <c.d.claxton@gmail.com>
2022-10-30 17:42:01 +0000

    Added quick note.

--- demo.sh
+++ demo.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 
-./app -data=./test-data-sets/set-1/data-config.json -i2=./test-data-sets/set-1/i2-config.json
+echo "Running Shortest Path App in demo mode"
+./app -data=./test-data-sets/set-1/data-config.json -i2=./test-data-sets/set-1/i2-config.json -folder=./working/
