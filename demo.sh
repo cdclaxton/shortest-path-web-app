0372f625fbdbfbb3d58e824694302caa3fa17c32
cdclaxton <c.d.claxton@gmail.com>
2022-11-09 20:34:05 +0000

    Added clean up of working folder.

--- demo.sh
+++ demo.sh
@@ -1,2 +1,9 @@
 echo "Running Shortest Path App in demo mode"
-./web-app -data=./test-data-sets/set-1/data-config.json -i2=./test-data-sets/set-1/i2-config.json -folder=./working/
+
+OUTPUT_FOLDER=./working/
+
+# Clean the working folder
+rm -f ./${OUTPUT_FOLDER}/*
+
+# Run the web-app
+./web-app -data=./test-data-sets/set-1/data-config.json -i2=./test-data-sets/set-1/i2-config.json -folder=${OUTPUT_FOLDER}
