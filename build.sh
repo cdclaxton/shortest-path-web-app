2b3ec1434bd78fb397cf324dbd1bbde6844212a3
cdclaxton <c.d.claxton@gmail.com>
2022-11-10 07:19:11 +0000

    Added exit codes.

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ testResult=$?
 
 if [[ $testResult -ne 0 ]]; then
     echo Tests failed. Stopping build.
-    exit
+    exit -1
 fi
 
 # Build the executable
@@ -16,6 +16,7 @@ buildResult=$?
 
 if [[ $buildResult -ne 0 ]]; then
     echo Build failed
+    exit -1
 else
     echo Build succeeded
 fi
