8b288768453df968328e20330de0b4e9b3012475
cdclaxton <c.d.claxton@gmail.com>
2022-12-04 08:37:37 +0000

    Improved script to create all necessary folders.

--- demo-pebble.sh
+++ demo-pebble.sh
@@ -1,15 +1,34 @@
 echo "Running Shortest Path App in demo mode (Pebble backend)"
 
 OUTPUT_FOLDER=./working/i2/
+BIPARTITE_FOLDER=./working/bipartitePebble
+UNIPARTITE_FOLDER=./working/unipartitePebble
+
+makeOrCleanFolder () {
+
+    # Preconditions
+    if [ $# -ne 2 ]; then
+        echo "Error: Incorrect number of arguments"
+        exit 1
+    fi
+
+    # Extract the arguments
+    folder=$1
+    name=$2
+
+    if [ ! -d ${folder} ]; then
+        echo "${name} folder doesn't exist. Making it at: ${folder}"
+        mkdir -p ${folder}
+    else    
+        echo "${name} folder found. Cleaning it"
+        rm -f ./${folder}/*
+    fi
+}
 
 # Make the output folder if it doesn't exist or clean it if it does
-if [[ ! -d ${OUTPUT_FOLDER} ]]; then
-    echo "i2 chart folder doesn't exist. Making it at: ${OUTPUT_FOLDER}"
-    mkdir -p ${OUTPUT_FOLDER}
-else 
-    echo "i2 chart folder found. Cleaning it"
-    rm -f ./${OUTPUT_FOLDER}/*
-fi
+makeOrCleanFolder ${OUTPUT_FOLDER} "i2 chart"
+makeOrCleanFolder ${BIPARTITE_FOLDER} "Bipartite graph"
+makeOrCleanFolder ${UNIPARTITE_FOLDER} "Unipartite graph"
 
 # Run the web-app
 ./web-app -data=./test-data-sets/set-1/data-config-pebble.json -i2=./test-data-sets/set-1/i2-config.json -folder=${OUTPUT_FOLDER}
