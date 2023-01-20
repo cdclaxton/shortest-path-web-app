38e02394289ef991d9e6cef47923f6b061388d8d
cdclaxton <c.d.claxton@gmail.com>
2023-01-20 10:37:44 +0000

    Parameterised dataset to use.

--- demo.sh
+++ demo.sh
@@ -2,6 +2,9 @@ echo "Running Shortest Path App in demo mode (in-memory)"
 
 OUTPUT_FOLDER=./working/i2/
 
+# Dataset within ./test-data-sets/ to use
+SET=set-1
+
 # Make the output folder if it doesn't exist or clean it if it does
 if [[ ! -d ${OUTPUT_FOLDER} ]]; then
     echo "i2 chart folder doesn't exist. Making it at: ${OUTPUT_FOLDER}"
@@ -12,4 +15,4 @@ else
 fi
 
 # Run the web-app
-./web-app -data=./test-data-sets/set-1/data-config.json -i2=./test-data-sets/set-1/i2-config.json -folder=${OUTPUT_FOLDER} -message=./test-data-sets/set-1/message.html
+./web-app -data=./test-data-sets/$SET/data-config.json -i2=./test-data-sets/$SET/i2-config.json -folder=${OUTPUT_FOLDER} -message=./test-data-sets/$SET/message.html
